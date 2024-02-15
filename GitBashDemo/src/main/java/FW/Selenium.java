b01257fce54a15e9d6f1c8ec569161836654d05d
maheshkolluru <maheshkolluru4@gmail.com>
2024-02-15 15:41:35 +0530

    SS

--- GitBashDemo/src/main/java/FW/Selenium.java
+++ GitBashDemo/src/main/java/FW/Selenium.java
@@ -4,6 +4,8 @@ public class Selenium {
 	
 	public static void main(String[] args) {
 		System.out.println("FW is ready");
+		System.out.println("FW is ready");
+
 	}
 
 }
---
+++ GitBashDemo/src/test/java/test/Tc1.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1 {
+	
+	@Test
+	public void tc1d()
+	{
+		Reporter.log("HH",true);
+	}
+
+}
