a801e44bb2b5699ec4ecef112dc080c8be058f07
maheshkolluru <maheshkolluru4@gmail.com>
2024-02-16 10:20:44 +0530

    DDDDDDD

---
+++ GitBashDemo/src/test/java/test/Tc2Test.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test {
+	@Test
+	public void m1()
+	{
+		Reporter.log("M1 Running",true);
+	}
+
+}
